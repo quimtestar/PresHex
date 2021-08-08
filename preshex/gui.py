bdf9688039c9b9d2f824a192145725bc5e1bb18f
Quim Testar <quimtestar@gmail.com>
2021-08-08 12:46:01 +0200

    NN model file selection.

--- preshex/gui.py
+++ preshex/gui.py
@@ -33,7 +33,7 @@ class BoardWidget(QWidget):
         self.board = board
         self.history = []
         self.historyPointer = 0
-        self.minimax = Minimax(heuristic = Predictor("model.h5",board.size).predict)
+        self.minimax = Minimax(heuristic = Predictor("model7.h5",board.size).predict) if board.size == 7 else Minimax()
         self.minimaxWorker = None
         
     def working(self):
